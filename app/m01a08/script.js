90d4e92c30b3d8499a744514e21227977311e0fd
Ivan A <iramaro@disroot.org>
2021-09-26 02:26:34 -0300

    Simple CSS in the file and input adjusted.

--- app/m01a08/script.js
+++ app/m01a08/script.js
@@ -1,16 +1,35 @@
 // catch the gost div below <h1>
-const listCollection = document.querySelector('[data-lists]')
-var list = ["Pineapple", "Banana", "Lemon", "Melon"]
+const listCollection = document.querySelector('[data-list]')
+
+const getForm =  document.querySelector('[data-new-list-form]')
+
+const getInput = document.querySelector('[data-new-list-input]')
+
+var list = []
+
+getForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const listInput = getInput.value
+  if([null, ''].includes(listInput)) return
+  const listData = createList(listInput)
+  getInput.value = null
+  list.push(listData)
+  render()
+}) 
 
 const render = () => {
   list.forEach(element => {
     const item = document.createElement('p')
-    let id = crypto.randomUUID()
-    item.id = id
-    item.innerText = `${id} - ${element}`
+    item.innerText = `${element.id} - ${element.data}`
     listCollection.appendChild(item)
-    id = null
+    list = []
   })
-  list = []
 }
-render(list)
+
+const createList = (listContent) => {
+  let contentObject = {
+    id: crypto.randomUUID(),
+    data: listContent
+  }
+  return contentObject
+}
